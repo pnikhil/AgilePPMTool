f8039352a4ece77010bab9e84058897092edfccf
Nikhil Naik <nnaik@mail.usf.edu>
2020-11-23 23:33:44 -0500

    added initial dashboard and header files

---
+++ src/components/Project/ProjectItem.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class ProjectItem extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Project Item</h2>
+            </div>
+        );
+    }
+}
+
+export default ProjectItem;
