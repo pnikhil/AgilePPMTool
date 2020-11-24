a62b4fc0b519e9508942e1df6463a9b5a5f3a01b
Nikhil Naik <nnaik@mail.usf.edu>
2020-11-23 19:04:10 -0500

    Added validation for Project model

--- src/main/java/io/fullstackexp/ppm/controller/ProjectController.java
+++ src/main/java/io/fullstackexp/ppm/controller/ProjectController.java
@@ -2,6 +2,7 @@ package io.fullstackexp.ppm.controller;
 
 import io.fullstackexp.ppm.domain.Project;
 import io.fullstackexp.ppm.service.ProjectService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/project")
 public class ProjectController {
@@ -20,6 +22,7 @@ public class ProjectController {
     @PostMapping("/create")
     public ResponseEntity<Project> createNewProject(@RequestBody Project project){
         Project responseProject = projectService.saveOrUpdateProject(project);
+        log.info("Ran the create new project method");
         return new ResponseEntity<>(responseProject, HttpStatus.CREATED);
     }
 }
--- src/main/java/io/fullstackexp/ppm/domain/Project.java
+++ src/main/java/io/fullstackexp/ppm/domain/Project.java
@@ -1,9 +1,11 @@
 package io.fullstackexp.ppm.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import javax.persistence.*;
-
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 import java.util.Date;
 
@@ -14,11 +16,24 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "Project Name is required")
     private String projectName;
+
+    @NotBlank(message = "Project Identifier is required")
+    @Size(min=4, max=5, message="Please use 4 to 5 characters")
+    @Column(updatable = false, unique = true)
     private String projectIdentifier; // Adding customer identifier for project object (JPA problem, so not using projectId)
+
+    @NotBlank(message = "Project Description is required")
     private String description;
+
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date startDate;
+
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date endDate;
+
     private LocalDateTime createdDateTime;
     private LocalDateTime updatedDateTime;
 
