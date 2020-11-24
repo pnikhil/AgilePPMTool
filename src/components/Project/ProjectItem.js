8ad3d6308c1be42707e9952013e9db93633272b0
Nikhil Naik <nnaik@mail.usf.edu>
2020-11-24 00:03:12 -0500

    added fontawesome and added html/css for styling

--- src/components/Project/ProjectItem.js
+++ src/components/Project/ProjectItem.js
@@ -1,10 +1,40 @@
 import React, {Component} from 'react';
-
+import { faFlag, faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 class ProjectItem extends Component {
     render() {
         return (
-            <div>
-                <h2>Project Item</h2>
+            <div className="container">
+                <div className="card card-body bg-light mb-3">
+                    <div className="row">
+                        <div className="col-2">
+                            <span className="mx-auto"> REACT</span>
+                        </div>
+                        <div className="col-lg-6 col-md-4 col-8">
+                            <h3>Spring / React Project</h3>
+                            <p>Project to create a Kanban Board with Spring Boot and React</p>
+                        </div>
+                        <div className="col-md-4 d-none d-lg-block">
+                            <ul className="list-group">
+                                <a href="#">
+                                    <li className="list-group-item board">
+                                        <FontAwesomeIcon icon={faFlag} /> Project Board
+                                    </li>
+                                </a>
+                                <a href="#">
+                                    <li className="list-group-item update">
+                                        <i className="pr-1 fa fa-edit"> <FontAwesomeIcon icon={faEdit} /> Update Project Info</i>
+                                    </li>
+                                </a>
+                                <a href="">
+                                    <li className="list-group-item delete">
+                                        <i className="pr-1 fa fa-minus-circle"> <FontAwesomeIcon icon={faTrash} /> Delete Project</i>
+                                    </li>
+                                </a>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
